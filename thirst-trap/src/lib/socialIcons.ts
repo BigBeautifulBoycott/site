5d9ecfde0d627cde056ff5e0841f0b61a21b6d5a
Bentley Hensel <bentleyhensel@gmail.com>
2025-10-28 19:23:04 -0400

    feat: social icons
    Signed-off-by: Bentley Hensel <bentleyhensel@gmail.com>

---
+++ thirst-trap/src/lib/socialIcons.ts
@@ -0,0 +1,10 @@
+export const SOCIAL_ICON_MAP: Record<string, string> = {
+  Instagram: "instagram.svg",
+  TikTok: "tiktok.svg",
+  Facebook: "facebook.svg",
+  LinkedIn: "linkedin.svg",
+  YouTube: "youtube.svg",
+  Twitter: "twitter.svg",
+  BlueSky: "bluesky.svg",
+  TruthSocial: "truthsocial.svg",
+};
